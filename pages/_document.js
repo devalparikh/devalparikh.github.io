4a92209744cee76a9d776b048a3c395b59bebb91
Deval Parikh <devalpp@gmail.com>
2022-08-07 22:05:38 -0400

    Nextjs build

--- pages/_document.js
+++ pages/_document.js
@@ -28,7 +28,7 @@ export default class MyDocument extends Document {
             name="keywords"
             content="Deval Parikh, deval parikh, devalparikh, devalparikh.me."
           />
-          <meta property="og:image" content="/img/devalparikhPreview.png" />
+
           <meta property="og:locale" content="en-US" />
           <meta property="og:type" content="website" />
           <meta
@@ -133,11 +133,8 @@ export default class MyDocument extends Document {
             href="https://lh3.googleusercontent.com/KUg3rbUWiVpV83Sdsy5FcsGeOlpcOgi1o3wLhcZGmGkjDmWCj4Hh4kLEEn_1b4dGsm9hzW7lGGvEsUpH2wpCRk9vlfod3ojbJyG7Rdb3ViBaBric9MwvP6J422dIifjzdT4gMJdKkWB4TIlfouuwVm9lxBgxoXEoGm3yiF7xMtFPNIEOTbbgG-WcV6I6jGhLj4AKoRJS_M2g2m5g132WKRTgWSLO4D4sL05lRPAATNq2h3bD72vFGm_Zk-r53dJGBe7FlHIcTV6rU85XD7wwF5sKtmddNbO9nbKg1L-W5pGcU-Ntvx9ayUmxMmGck0FXhf3saN6Qys_dh_C31jy1StMNeceswq1cH8712PvVmca1uNircDdNUbgKIstcn2Q3Egw1w_P5fJ17H6Ru7ZB8gmgEtivx8enUeE_TknyYz37p71GEGCg9-E2m5bl9YaUNTnODjQ2Lh8xeXYnsnCFkvw-XTlWFi799mxtjQFbPK32oAbV728dCld7eB8oTcvB4msAah-hukPKOr83q4GeiZlUe-w_L9hXzFDXF5d86CQbD7ypeUnlhdMaZi8KXOXrkEUMD_njXWSFEh9Xdcnt87Yvkz7VqRxbDHUz4Nid5oiUFAYtUgV917fg1PHroY1Iy0R3g97GXAxHsVc0PfzKLcKgZvoDVURscN8jlpDvbwGfiJ_4HwRHFRys=w1080-h1440-no"
           />
           <meta property="og:title" content="Deval Parikh" />
-          <meta property="og:url" content="https://devalparikh.me" />
-          <meta
-            property="og:image"
-            content="https://i.ibb.co/jz5Qwx1/Group-7.png"
-          />
+          <meta property="og:url" content="https://devalparikh.github.io/" />
+          <meta property="og:image" content="/img/devalparikhPreview.png" />
           <meta name="apple-mobile-web-app-title" content="Deval Parikh" />
           <meta name="next-head-count" content="21" />
           <style
