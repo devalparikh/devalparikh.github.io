fee98e3248077da9fc30ae210985ba1bde907c21
Deval Parikh <devalpp@gmail.com>
2022-08-07 22:08:52 -0400

    Nextjs build

--- pages/_document.js
+++ pages/_document.js
@@ -43,10 +43,10 @@ export default class MyDocument extends Document {
           <meta name="mobile-web-app-capable" content="yes" />
           <meta
             name="msapplication-config"
-            content="/icons/browserconfig.xml"
+            content="./icons/browserconfig.xml"
           />
-          <meta name="twitter:site" content="" />
-          <meta name="twitter:image" content="/img/devalparikhPreview.png" />
+
+          <meta name="twitter:image" content="./img/devalparikhPreview.png" />
           <link
             rel="preload"
             as="style"
@@ -54,79 +54,82 @@ export default class MyDocument extends Document {
           />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="./favicon.ico" />
           <link
             rel="apple-touch-icon"
             sizes="57x57"
-            href="/icons/apple-icon-57x57.png"
+            href="./icons/apple-icon-57x57.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="60x60"
-            href="/icons/apple-icon-60x60.png"
+            href="./icons/apple-icon-60x60.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="72x72"
-            href="/icons/apple-icon-72x72.png"
+            href="./icons/apple-icon-72x72.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/icons/apple-icon-76x76.png"
+            href="./icons/apple-icon-76x76.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="114x114"
-            href="/icons/apple-icon-114x114.png"
+            href="./icons/apple-icon-114x114.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="120x120"
-            href="/icons/apple-icon-120x120.png"
+            href="./icons/apple-icon-120x120.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="144x144"
-            href="/icons/apple-icon-144x144.png"
+            href="./icons/apple-icon-144x144.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="152x152"
-            href="/icons/apple-icon-152x152.png"
+            href="./icons/apple-icon-152x152.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/icons/apple-icon-180x180.png"
+            href="./icons/apple-icon-180x180.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
-            href="/icons/android-icon-192x192.png"
+            href="./icons/android-icon-192x192.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/icons/favicon-32x32.png"
+            href="./icons/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="96x96"
-            href="/icons/favicon-96x96.png"
+            href="./icons/favicon-96x96.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/icons/favicon-16x16.png"
+            href="./icons/favicon-16x16.png"
           />
-          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="msapplication-TileImage"
+            content="./ms-icon-144x144.png"
+          />
+          <link rel="icon" href="./favicon.ico" />
+          <link rel="manifest" href="./manifest.json" />
           <meta name="robots" content="index, nofollow" />
           <link
             rel="author"
@@ -134,7 +137,7 @@ export default class MyDocument extends Document {
           />
           <meta property="og:title" content="Deval Parikh" />
           <meta property="og:url" content="https://devalparikh.github.io/" />
-          <meta property="og:image" content="/img/devalparikhPreview.png" />
+          <meta property="og:image" content="./img/devalparikhPreview.png" />
           <meta name="apple-mobile-web-app-title" content="Deval Parikh" />
           <meta name="next-head-count" content="21" />
           <style
@@ -174,8 +177,8 @@ export default class MyDocument extends Document {
             `,
             }}
           />
-          <link rel="stylesheet" href="/css/normalize.css" />
-          <link rel="stylesheet" href="/css/base.css" />
+          <link rel="stylesheet" href="./css/normalize.css" />
+          <link rel="stylesheet" href="./css/base.css" />
         </Head>
         <body>
           <Main />
