ecc4f1cfa7c000bd37d498512db3c32c2d5b2570
Deval Parikh <devalpp@gmail.com>
2022-08-07 22:44:48 -0400

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8729ca7c061f6867fa30";
+/******/ 	var hotCurrentHash = "c478a218d8a85a363e3e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
