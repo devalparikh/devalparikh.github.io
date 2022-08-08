5a69800a6b6248d8d1368d8ac5ddf41540543b82
devalparikh <devalparikh@users.noreply.github.com>
2022-08-08 04:03:10 +0000

    deploy: 542ff62830f4702c49435c5d96b1727f185cc55a

---
+++ _next/static/chunks/pages/projects-e25a3d78e3153bbb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327],{9182:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/projects",function(){return u(8997)}])}},function(n){n.O(0,[774,297,483,747,997,888,179],(function(){return _=9182,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
